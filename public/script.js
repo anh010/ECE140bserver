cdeaaca79f9e5cdc866480f30880be461abde6c7
anh010 <anh010@ucsd.edu>
2022-11-19 14:45:56 -0800

    Revert "Deploying to lint from @ anh010/ECE140bserver@6374d96e8c13c902ad3be265caf64ce37dba1102 🚀"
    This reverts commit faa09ae8f15d5b3d75c4932be9f063e4bba69b5f.

---
+++ public/script.js
@@ -0,0 +1,59 @@
+
+/**
+ * this function will accept detials about a person 
+ * create an object for them.
+ * 
+ * @param {string} name the persons full name 
+ * @param {number} age the persons age
+ * @param {boolean} isDeveloper whether or not person is developer
+ * @returns {Object} the person object 
+ */
+function createPerson(name, age, isDeveloper){
+    return{
+        name: name,
+        age: age,
+        isDeveloper:isDeveloper
+    }
+}
+
+class Person {
+    /**
+    * this constructor creates a person
+    * 
+    * @param {string} name the persons full name 
+    * @param {number} age the persons age
+    * @param {boolean} isDeveloper whether or not person is developer
+    * @returns {Object} the person object 
+    */
+    constructor(name, age, isDeveloper){
+        this.name=name;
+        this.age=age;
+        this.isDeveloper=isDeveloper;
+    }
+}
+
+/**
+ * Outpups etails abouta person. 
+ * 
+ * @param {Person} person 
+ */
+function printDetails(person){
+    console.log(`${person.name} is ${person.age} years old is `)
+}
+
+
+/**
+ * gives you numbers.
+ * 
+ * @returns {number[]} 
+ */
+function getNumber(){
+    return[10,12,130,150]
+}
+const numbers = getNumber();
+
+for(const n in numbers){
+    console.log(n);
+}
+let p1 = new Person('alex',21,true)
+printDetails(p1);
