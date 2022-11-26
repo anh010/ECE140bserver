2fd182ce20428da3c589040ce858bcdca8547240
anh010 <anh010@ucsd.edu>
2022-11-26 14:16:20 -0800

    update ugly.js

--- public/ugly.js
+++ public/ugly.js
@@ -2,3 +2,10 @@
  * function that does nothings
  */
 function dosomething(){console.log("this is horrible code");}
+
+/**
+ * horribly formated sum function
+ * @param {*} sum1 
+ * @param {*} sum2 
+ */
+function anotherUGly( sum1,sum2){console.log(sum1+sum2);}
