f026932c6a592da24f5df73d9680009ade798344
anh010 <anh010@ucsd.edu>
2022-11-28 14:16:25 -0800

    test 2 with branch protection

--- public/ugly.js
+++ public/ugly.js
@@ -4,3 +4,13 @@
 function dosomething() {
   console.log("this is horrible code");
 }
+
+/**
+ * 
+ * @param {number} sum1 
+ * @param {number} sum2 
+ */
+function anotherUglyone(sum1,sum2)
+{console.log(sum1+        sum2);
+    let something =1;
+}
